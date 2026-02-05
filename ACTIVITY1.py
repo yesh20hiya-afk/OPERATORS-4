7ff31ddafdb6a9cbaba793c0a777a0b3ee3844c8
yesh20hiya-afk <yesh20hiya@gmail.com>
2026-02-04 20:24:53 -0500

    v1

---
+++ ACTIVITY1.py
@@ -0,0 +1,16 @@
+v = 4  
+w = 6
+x = 8
+y = 2
+z = 0
+z = (v + w) - x * y
+print("The value of (v + w) - x * y is:", z)
+
+
+name = "Alex"
+age = 0
+
+if name == "Alex" or name == "John" and age >= 2:
+    print("Hello, welcome")
+else:
+    print("goodbye")
